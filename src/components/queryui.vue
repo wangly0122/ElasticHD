71b546e17b42dc6a89ee682a9f7ed26976caba1c
DoloresY <yaoli199@163.com>
2019-04-20 17:40:38 +0800

    新增monitor&queryui组件 & 添加路由

---
+++ src/components/queryui.vue
@@ -0,0 +1,41 @@
+<template>
+  <section id="sqltools">
+      
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'sqltools',
+  data: function () {
+    return {
+      textarea: 'SELECT * FROM myindex',
+      activeName: 1
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'dslBody',
+      'sqlBody',
+      'currentError',
+      'serverhost'
+    ])
+  },
+  created: function () {
+  },
+  mounted () {
+  },
+  methods: {
+    sqlExplain () {
+      var tmpVal = {url: '/_sql2dsl', sql: this.textarea, host: this.serverhost}
+      this.$store.dispatch('ConvertSQLToDSL', tmpVal)
+      document.getElementById('dslBody').style.display = 'block'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home.vue'
+import OverView from '@/components/overview'
+import Search from '@/components/search.vue'
+import Indices from '@/components/indices.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +13,21 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: OverView
+    },
+    {
+      path: '/query',
+      name: 'query',
+      component: Search
+    },
+    {
+      path: '/indices',
+      name: 'indices',
+      component: Indices
     }
   ]
 })
